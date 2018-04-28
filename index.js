c94d566f5e5591db18a1b3f5199ce2787d36a7ed
Scriptim <Scriptim@gmx.de>
2018-04-28 21:32:22 +0200

    dynamically loading plugins

--- index.js
+++ index.js
@@ -12,9 +12,52 @@ const reddit = new snoowrap({
 
 const subreddit = reddit.getSubreddit(credentials.REDDIT_SUBREDDIT)
 
-const dummy = require('./plugins/dummy/index.js')
 const api = {
   reddit: subreddit
 }
-dummy.setup(api)
-dummy.run(api)
+
+const fs = require('fs')
+const path = require('path')
+
+let items
+try {
+  items = fs.readdirSync('./plugins')
+} catch (ex) {
+  console.error('An error occured while reading the plugins directory')
+  process.exit(1)
+}
+
+let plugins = []
+for (let item of items) {
+  let pluginEntry = path.resolve(path.join('./plugins', item, 'index.js'))
+  let plugin
+
+  try {
+    plugin = require(pluginEntry)
+    console.log('Loaded plugin \'' + plugin.name
+      + '\' by \'' + plugin.author
+      + '\' [' + plugin.interval + ']')
+  } catch (err) {
+    console.error('Could not load plugin from "' + pluginEntry + '"')
+    continue
+  }
+
+  try {
+    plugin.setup(api)
+  } catch (err) {
+    console.error('Setup failed for plugin \'' + plugin.name + '\'')
+    continue
+  }
+
+  plugins.push(plugin)
+}
+
+for (let plugin of plugins) {
+  setInterval(() => {
+    try {
+      plugin.run(api)
+    } catch(ex) {
+      console.error('An error occured while running \'' + plugin.name + '\'')
+    }
+  }, plugin.interval * 1000)
+}
