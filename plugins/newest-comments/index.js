d0c5b0d7c404261db167a5eedb8b962efee56003
Scriptim <Scriptim@gmx.de>
2018-04-29 22:17:47 +0200

    created index.js with meta information

---
+++ plugins/newest-comments/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'Newest Comments',
+  author: 'Scriptim',
+  description: 'Update a post containing the most recent comments on the subreddit',
+  interval: 5,
+  setup: (api, logger) => {
+ 
+  },
+  run: (api, logger) => {
+
+  }
+}
