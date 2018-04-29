381405ac806daf3f3dd1cd5cc64be92c1fab498b
Scriptim <Scriptim@gmx.de>
2018-04-29 23:28:04 +0200

    created index.js with meta information

---
+++ plugins/coinmarketcap/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'Coinmarketcap',
+  author: 'Scriptim',
+  description: 'Update a post with the current market cap of several crypto currencies',
+  interval: 60,
+  setup: (api, logger) => {
+  
+  },
+  run: (api, logger) => {
+    
+  }
+}
