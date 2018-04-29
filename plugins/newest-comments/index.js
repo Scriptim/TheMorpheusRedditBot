a5e8e0f563bbadad6ef68b9ec350a520e24d23e8
Scriptim <Scriptim@gmx.de>
2018-04-29 22:27:57 +0200

    formatting and posting comments

--- plugins/newest-comments/index.js
+++ plugins/newest-comments/index.js
@@ -1,6 +1,30 @@
 const request = require('request')
 let post
 
+function humanReadableDateDiff(timestamp) {
+  // difference in seconds
+  const diff = Math.floor((new Date() - new Date(timestamp * 1000)) / 1000)
+
+  const units = {
+    'Jahr': [31536000, 'e'],
+    'Monat': [2592000, 'e'],
+    'Tag': [86400, 'en'],
+    'Stunde': [3600, 'n'],
+    'Minute': [60, 'n']
+  }
+
+  for (let unit in units) {
+    let n = Math.floor(diff / units[unit][0])
+    if (n == 1) {
+      return n + ' ' + unit
+    } else if (n > 1) {
+      return n + ' ' + unit + units[unit][1] // append plural letter(s)
+    }
+  }
+
+  return 'gerade eben' // less than 1 minute
+}
+
 module.exports = {
   name: 'Newest Comments',
   author: 'Scriptim',
@@ -27,6 +51,32 @@ module.exports = {
       }
 
       const comments = JSON.parse(body).data
+      const childStrings = []
+
+      // format comments
+      for (let child of comments.children) {
+        child = child.data
+        let childStr = ''
+        childStr += '**[Kommentar](' + child.permalink + ')'
+        childStr += ' von u/' + child.author
+        childStr += ' unter [' + child.link_title + '](' + child.link_permalink + ')**'
+        childStr += '  \n>' + child.body.replace(/\n/g, '\n>') + '  \n\n'
+        // &#183; = middle dot for separation
+        childStr += '**&nbsp;&#11137; ' + child.score + '&nbsp;&#183;&nbsp;' // up/down arrows
+        childStr += 'vor ' + humanReadableDateDiff(child.created_utc) + '&nbsp;&#183;&nbsp;'
+        childStr += child.edited ? '&#128393;&nbsp;&nbsp;' : '' // pencil
+        childStr += child.stickied ? '&#128204;&nbsp;&nbsp;' : '' // pushpin
+        childStr += child.approved ? '&#10003;&nbsp;&nbsp;' : '' // tick
+        childStr += child.removed ? '&#128465;&nbsp;&nbsp;' : '' // wastebasket
+        childStr += '**'
+
+        childStrings.push(childStr)
+      }
+
+      const commentsString = childStrings.join('\n\n---\n\n')
+
+      logger.debug('Editing post')
+      post.edit(commentsString)
     })
   }
 }
