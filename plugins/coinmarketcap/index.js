5a512ede9c0fd0eee9d4a57d0cb611c05f2cfd27
Scriptim <Scriptim@gmx.de>
2018-04-29 23:54:44 +0200

    fetching, formatting and posting market caps

--- plugins/coinmarketcap/index.js
+++ plugins/coinmarketcap/index.js
@@ -1,4 +1,13 @@
+const request = require('request')
 let post
+
+function prepenPlusIfPositive(percentage) {
+  // prepends '+' if percentage is not negative
+  if (percentage[0] !== '-') {
+    percentage = '+' + percentage
+  }
+  return percentage
+}
 
 module.exports = {
   name: 'Coinmarketcap',
@@ -11,6 +20,36 @@ module.exports = {
     post = api.reddit.getSubmission(postId)
   },
   run: (api, logger) => {
-    
+    logger.debug('Requesting comments')
+    // Receive top 50 cryptocurrency market capitalizations
+    request('https://api.coinmarketcap.com/v1/ticker/?limit=50&convert=EUR', (err, res, body) => {
+      if (err) {
+        logger.error(err)
+      }
+
+      if (res) {
+        logger.debug('Received response with status code ' + res.statusCode)
+      } else {
+        logger.error(new Error('No response received'))
+        return
+      }
+
+      const coins = JSON.parse(body)
+      const coinStrings = []
+      for (let coin of coins) {
+        let coinString = ''
+        coinString += coin.rank + '. '
+        coinString += '**' + coin.name + '** (' + coin.symbol + ')  \n'
+        coinString += '**' + coin.price_eur + '&euro;** '
+        coinString += '(' + prepenPlusIfPositive(coin.percent_change_1h) + '%/Stunde, '
+        coinString += prepenPlusIfPositive(coin.percent_change_24h) + '%/Tag, '
+        coinString += prepenPlusIfPositive(coin.percent_change_7d) + '%/Woche)'
+
+        coinStrings.push(coinString)
+      }
+
+      logger.debug('Editing post')
+      post.edit(coinStrings.join('\n'))
+    })
   }
 }
