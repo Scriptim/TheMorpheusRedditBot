fee76bfbbc03fe016af86557ec0d1ffe7308ca93
Scriptim <Scriptim@gmx.de>
2018-04-29 22:25:29 +0200

    getting submission

--- plugins/newest-comments/index.js
+++ plugins/newest-comments/index.js
@@ -1,10 +1,14 @@
+let post
+
 module.exports = {
   name: 'Newest Comments',
   author: 'Scriptim',
   description: 'Update a post containing the most recent comments on the subreddit',
   interval: 5,
   setup: (api, logger) => {
- 
+    const postId = '8ft5jv'
+    logger.debug('Getting submission by id ' + postId)
+    post = api.reddit.getSubmission(postId)
   },
   run: (api, logger) => {
 
