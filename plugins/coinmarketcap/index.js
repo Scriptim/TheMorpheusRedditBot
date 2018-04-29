790d431efe10515d1089bad06f8092b46a2c919e
Scriptim <Scriptim@gmx.de>
2018-04-29 23:33:58 +0200

    getting submission

--- plugins/coinmarketcap/index.js
+++ plugins/coinmarketcap/index.js
@@ -1,10 +1,14 @@
+let post
+
 module.exports = {
   name: 'Coinmarketcap',
   author: 'Scriptim',
   description: 'Update a post with the current market cap of several crypto currencies',
   interval: 60,
   setup: (api, logger) => {
-  
+    const postId = '8fugur'
+    logger.debug('Getting submission by id ' + postId)
+    post = api.reddit.getSubmission(postId)
   },
   run: (api, logger) => {
     
