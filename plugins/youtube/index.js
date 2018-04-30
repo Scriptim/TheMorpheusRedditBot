252ff468a60d0f5169f6e2e120210cc943ae5524
Scriptim <Scriptim@gmx.de>
2018-04-30 22:56:38 +0200

    created index.js with meta information

---
+++ plugins/youtube/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  name: 'YouTube',
+  author: 'Scriptim',
+  description: 'Display some information about "The Morpheus Tutorials" YouTube Channel',
+  interval: 300,
+  setup: (api, logger) => {
+  
+  },
+  run: (api, logger) => {
+    
+  }
+}
