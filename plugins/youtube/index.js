2ef89203cebe3169e7427efad70bff4c90f4f302
Scriptim <Scriptim@gmx.de>
2018-04-30 23:01:21 +0200

    getting submission

--- plugins/youtube/index.js
+++ plugins/youtube/index.js
@@ -1,10 +1,14 @@
+let post
+
 module.exports = {
   name: 'YouTube',
   author: 'Scriptim',
   description: 'Display some information about "The Morpheus Tutorials" YouTube Channel',
   interval: 300,
   setup: (api, logger) => {
-  
+    const postId = '8g2uyv'
+    logger.debug('Getting submission by id ' + postId)
+    post = api.reddit.getSubmission(postId)
   },
   run: (api, logger) => {
     
