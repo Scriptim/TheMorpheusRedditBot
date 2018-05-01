908dfb7d8f51325cb8c2ec336b128a80b73ba044
Scriptim <Scriptim@gmx.de>
2018-05-01 15:36:56 +0200

    requesting newest video before statistics

--- plugins/youtube/index.js
+++ plugins/youtube/index.js
@@ -36,11 +36,17 @@ module.exports = {
     post = api.reddit.getSubmission(postId)
   },
   run: (api, logger) => {
-    logger.debug('Requesting statistics')
-    api.youtube.channels.list({
-      part: 'statistics',
-      id: channelId,
-      maxResults: 1
+    let text = ''
+
+    // searching for videos as youtube data api does not provide
+    // a convenient way for getting most recent video
+    logger.debug('Requesting search results')
+    api.youtube.search.list({
+      part: 'snippet',
+      channelId: channelId,
+      maxResults: 1,
+      order: 'date',
+      type: 'video'
     }, (err, res) => {
       if (err) {
         logger.error(err)
@@ -54,26 +60,21 @@ module.exports = {
         return
       }
 
-      const channel = res.data.items[0]
+      const video = res.data.items[0]
 
-      let text = ''
+      // most recent video
+      text += '# Neuestes Video\n'
+      text += '[**' + video.snippet.title + '**]'
+      text += '(https://www.youtube.com/watch?v=' + video.id.videoId + ')  \n'
+      text += '*' + humanReadableDateDiff(video.snippet.publishedAt) + '*  \n'
+      text += '>' + video.snippet.description.replace(/\n/g, '  \n').split('\n').join('\n>')
+      text += '\n'
 
-      // statistics
-      const stats = channel.statistics
-      text += '# Statistiken\n'
-      text += '**' + stats.subscriberCount + '** Abonnenten  \n'
-      text += '**' + stats.viewCount + '** Views  \n'
-      text += '**' + stats.videoCount + '** Videos\n\n'
-
-      // searching for videos as youtube data api does not provide
-      // a convenient way for getting most recent video
-      logger.debug('Requesting search results')
-      api.youtube.search.list({
-        part: 'snippet',
-        channelId: channelId,
-        maxResults: 1,
-        order: 'date',
-        type: 'video'
+      logger.debug('Requesting statistics')
+      api.youtube.channels.list({
+        part: 'statistics',
+        id: channelId,
+        maxResults: 1
       }, (err, res) => {
         if (err) {
           logger.error(err)
@@ -87,18 +88,14 @@ module.exports = {
           return
         }
 
-        const video = res.data.items[0]
+        const channel = res.data.items[0]
 
-        // most recent video
-        let newestVideo = ''
-        newestVideo += '# Neuestes Video\n'
-        newestVideo += '[**' + video.snippet.title + '**]'
-        newestVideo += '(https://www.youtube.com/watch?v=' + video.id.videoId + ')  \n'
-        newestVideo += '*' + humanReadableDateDiff(video.snippet.publishedAt) + '*  \n'
-        newestVideo += '>' + video.snippet.description.replace(/\n/g, '  \n').split('\n').join('\n>')
-        newestVideo += '\n'
-
-        text = newestVideo + text
+        // statistics
+        const stats = channel.statistics
+        text += '# Statistiken\n'
+        text += '**' + stats.subscriberCount + '** Abonnenten  \n'
+        text += '**' + stats.viewCount + '** Views  \n'
+        text += '**' + stats.videoCount + '** Videos\n\n'
 
         logger.debug('Editing post')
         post.edit(text)
